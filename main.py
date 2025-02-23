945ef8051a0a265ea762c6f7892ea8a4f6a16e52
JMar22 <juan.martinez.2488@gmail.com>
2025-02-22 16:58:21 -0800

    now uses sys.argv to get path of book

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-def main():
-    filepath = "books/frankenstein.txt"
-    file_contents = read_file(filepath)
-    word_count = count_words(file_contents)
-    character_ocurrences = count_character_ocurrences(file_contents)
-    sorted_list_of_dictionaries = create_sorted_list_of_dictionaries(character_ocurrences)
-    print(f"--- Begin report of {filepath} ---")
-    print(f"{word_count} words found in the document")
-    print()
-    for dictionary in sorted_list_of_dictionaries:
-        if dictionary["character"].isalpha():
-            print(f"The '{dictionary['character']}' character was found {dictionary['count']} times")
-    print("--- End report ---")
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
-def read_file(filepath):
-    with open(filepath) as f:
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
         return f.read()
 
-def count_words(string):
-    return len(string.split())
 
-def count_character_ocurrences(string):
-    character_occurrences = {}
-    for character in string:
-        if character.lower() not in character_occurrences:
-            character_occurrences[character.lower()] = 1
-        else:
-            character_occurrences[character.lower()] += 1
-    return character_occurrences
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
 
-def create_sorted_list_of_dictionaries(dictionary):
-    sorted_list_of_dictonaries = []
-    for character in dictionary:
-        sorted_list_of_dictonaries.append({"character":character, "count":dictionary[character]})
+    print("============= END ===============")
 
-    def return_value(dictionary):
-        return dictionary["count"]
-    sorted_list_of_dictonaries.sort(reverse=True, key=return_value)
-    return sorted_list_of_dictonaries
 
 main()
